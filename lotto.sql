4313346d0d503509cea6c1aef2d5d4f332c083a4
ypy02784 <ypy02784@163.com>
2019-03-14 16:04:12 +0800

    get data from web

---
+++ lotto.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE `dlt`  (
+  `phasenum` int(11) ,
+  `date` text(255),
+  `red1` int(2) ,
+  `red2` int(2) ,
+  `red3` int(2)  ,
+  `red4` int(2)  ,
+  `red5` int(2)  ,
+  `blue1` int(2)  ,
+  `blue2` int(2)  ,
+  `jackpot` int(30)  ,
+  `firstnum` int(2)  ,
+  `firstprice` int(8)  ,
+  `totalbet` int(30)
+    
+) 
